11ff269dfb651f0cd2f4797504b8f6f4b6120e12
Michel Sommer <skruen@gmail.com>
2022-12-15 15:01:35 +0100

    init

--- MyMovieGetItemData/MyMovieGetItemData.cs
+++ MyMovieGetItemData/MyMovieGetItemData.cs
@@ -1,33 +1,46 @@
 using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Data.SqlClient;
-using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Company.Function
 {
     public static class MyMovieGetItemData
     {
         [FunctionName("MyMovieGetItemData")]
-        public static async Task<IActionResult> Run(
-                [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req, int itemId)
+        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] int itemId)
         {
-
             string queryString = $"SELECT * FROM [dbo].[userdata] WHERE itemId = '{itemId}';";
+            var message = String.Empty;
 
             using (SqlConnection connection = new SqlConnection("ConnectionString"))
             {
                 SqlCommand command = new SqlCommand(queryString, connection);
                 command.Connection.Open();
-                command.ExecuteNonQuery();
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            var json = $"\"timeStamp\": = \"{reader.GetInt32(0)}\"," +
+                                       $"\"userName\": = \"{reader.GetString(1)}\"," +
+                                       $"\"listName\": = \"{reader.GetString(2)}\"," +
+                                       $"\"itemId\": = \"{reader.GetInt32(3)}\"," +
+                                       $"\"comment\": = \"{reader.GetString(4)}\"," +
+                                       $"\"count\": = \"{reader.GetInt32(5)}\";";
+
+                            var jsonString = "{" + json + "}";
+                            message = String.Format(jsonString);
+                        }
+                    }
+
+                    return (ActionResult)new OkObjectResult(message);
+                }
             }
-
-            return new OkObjectResult("200");
-
         }
-    }
+    }                        
 }
