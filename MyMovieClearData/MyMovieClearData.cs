b0f9df86a17e1bf5eadfb260879536629f42abdc
Michel Sommer <skruen@gmail.com>
2022-12-15 01:21:32 +0100

    init

---
+++ MyMovieClearData/MyMovieClearData.cs
@@ -0,0 +1,30 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Data.SqlClient;
+
+namespace Company.Function
+{
+    public static class MyMovieClearData
+    {
+    [FunctionName("MyMovieClearData")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req, string userName)
+        {
+
+            string queryString = $"DELETE * FROM [dbo].[userdata] WHERE userName = '{userName}';";
+
+            using (SqlConnection connection = new SqlConnection("ConnectionString"))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Connection.Open();
+                command.ExecuteNonQuery();
+            }
+
+            return new OkObjectResult("200");
+
+        }
+    }
+}
